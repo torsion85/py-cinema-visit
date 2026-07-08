7e9f7ae3e71543df0678a93a2e5b403f7409cc4b
Andrii Cherniienko <chern.combat32@gmail.com>
2026-07-08 19:00:03 +0300

    Added isinstance

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ from app.people.cinema_staff import Cleaner
 def cinema_visit(movie: str,
                  customers: list,
                  hall_number: int,
-                 cleaner: str,
-                 ) -> None:
+                 cleaner: str,) -> None:
     result = []
+
+    if isinstance(movie, list):
+        customers, hall_number, cleaner, movie = movie, customers, hall_number, cleaner
 
     for person in customers:
         customer = Customer(name=person["name"], food=person["food"])
