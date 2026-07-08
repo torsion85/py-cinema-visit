8979e8f2f87ee25359b647725d88c4843caf59a0
Andrii Cherniienko <chern.combat32@gmail.com>
2026-07-08 16:33:14 +0300

    Fixed

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,5 +4,5 @@ from app.people.customer import Customer
 class CinemaBar:
 
     @staticmethod
-    def sell_product(product: str, customer: Customer) -> str:
+    def sell_product(product: str, customer: Customer) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -2,5 +2,5 @@ class Cleaner:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def clean_hall(self, hall_number: int) -> str:
+    def clean_hall(self, hall_number: int) -> None:
         print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
